5c008b3fa5c9367ddb8e976f79dc5b04bfe38c74
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-12-13 14:45:46 +0100

    fix: Updating category and categories components

--- src/components/category/index.tsx
+++ src/components/category/index.tsx
@@ -2,6 +2,8 @@ import { Text, Pressable, PressableProps } from 'react-native';
 
 import { s } from "./styles";
 
+import { colors } from "@/styles/theme";
+
 import { categoriesIcons } from '@/utils/categories-icons';
 
 type Props  = PressableProps & {
@@ -14,9 +16,12 @@ export function Category({ name, iconId, isSelected = false, ...rest }: Props) {
     const Icon = categoriesIcons[iconId];
 
     return (
-        <Pressable style={[s.container]}>
-            <Icon size={16} />
-            <Text style={[s.name]}>{name}</Text>
+        <Pressable
+            style={[s.container, isSelected && s.containerSelected]}
+            {...rest}
+        >
+            <Icon size={16} color={colors.gray[isSelected ? 100 : 400]}/>
+            <Text style={[s.name , isSelected && s.nameSelected]}>{name}</Text>
         </Pressable>
     )
 };
