7df16560c69ee43ae3e2b7649bae387ee0f1e178
Victor Henrique de Camargo Nestali <victornestali@gmail.com>
2024-12-13 14:46:46 +0100

    fix: Updating home and _layout files in app folder and adding libraries

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,4 +1,4 @@
-import { View, Alert, Text } from "react-native";
+import { View, Alert } from "react-native";
 
 import { api } from "@/services/api";
 
@@ -6,29 +6,60 @@ import { useEffect, useState } from "react";
 
 import { Categories, CategoriesProps} from "@/components/categories";
 
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+
+type MarketsProps = PlaceProps;
+
 
 export default function Home() {
     const [categories, setCategories] = useState<CategoriesProps>([]);
+    const [category, setCategory] = useState("");
     
-    
+    const [markets, setMarkets] = useState<MarketsProps[]>([]);
     
     async function fetchCategories() {
         try {
             const { data } = await api.get('/categories');
             setCategories(data);
+            setCategory(data[0].id)
         } catch (error) {
             console.log(error);
             Alert.alert("Categorias", "Não foi possível carregas as categorias.");
         }
     };
 
+    async function fetchMarkets() {
+        try {
+            if(!category) {
+                return
+            };
+
+            const { data } = await api.get('/markets/category/' + category);
+            setMarkets(data);
+        } catch (error) {
+            console.log(error);
+            Alert.alert("Locais", "Não foi possível carregar os locais.");
+        }
+    };
+
     useEffect(() => {
         fetchCategories();
-    }, [])
+    }, []);
+
+    useEffect(() => {
+        fetchMarkets();
+    }, [category]);
     
     return (
-        <View style={{ flex: 1 }}>
-            <Categories data={categories}/>
+        <View style={{ flex: 1, backgroundColor: "#CECECE" }}>
+            <Categories 
+                data={categories} 
+                onSelect={setCategory} 
+                selected={category} 
+            />
+
+            <Places data={markets} />
         </View>
     )
 };
